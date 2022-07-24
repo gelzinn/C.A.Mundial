5212cc16519ae3cf9ff71ca310f9ff36e1ace7a3
gelzin <mcnoobend@gmail.com>
2022-07-24 15:08:06 -0300

    users list

--- src/pages/dashboard/users.tsx
+++ src/pages/dashboard/users.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import Aside from "~/components/Dashboard/Aside";
+import LoadingCircle from "~/components/LoadingCircle";
 import { db } from "~/services/firebase";
 import { GridAppContainer } from "~/styles/pages/dashboard";
 
@@ -20,18 +21,24 @@ export default function Users({ users }) {
               <span>Veja todos os</span>
               <h1>USUÁRIOS</h1>
             </div>
-            {users[0].map((user, i) => {
-              console.log(user);
+            {users ? (
+              <>
+                {users[0].map((user, i) => {
+                  console.log(user);
 
-              return (
-                <li key={i}>
-                  <p>{user.name}</p>
-                  <p>{user.email}</p>
-                  <p>{user.phone}</p>
-                  {user.admin && <p>Admininstrador</p>}
-                </li>
-              );
-            })}
+                  return (
+                    <li key={i}>
+                      <p>{user.name}</p>
+                      <p>{user.email}</p>
+                      <p>{user.phone}</p>
+                      {user.admin && <p>Admininstrador</p>}
+                    </li>
+                  );
+                })}
+              </>
+            ) : (
+              <LoadingCircle />
+            )}
           </>
         </main>
       </GridAppContainer>
